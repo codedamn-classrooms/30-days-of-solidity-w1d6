491d6a7b745d52de1e384892d5844a8191a6de9f
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-04-04 17:29:39 +0530

    Update and rename day6.sol to day1.sol

--- contracts/day1.sol
+++ contracts/day1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: aIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract day6 {
+contract day1 {
     function digitSum(int256 n) public pure returns (int256) {
         int256 a;
         int256 sum = 0;
