b9c202855e894d74d549c44bc3e578182de56b9f
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-04-04 17:30:03 +0530

    Update and rename 1_day6.js to 1_day1.js

---
+++ migrations/1_day1.js
@@ -0,0 +1,5 @@
+const Day1 = artifacts.require("day1");
+
+module.exports = function (deployer) {
+  deployer.deploy(Day1);
+};
--- migrations/1_day6.js
+++
@@ -1,5 +0,0 @@
-const Day6 = artifacts.require("day6");
-
-module.exports = function (deployer) {
-  deployer.deploy(Day6);
-};
